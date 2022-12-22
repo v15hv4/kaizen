bc5185bbfb89ec9130f6bf75e44faddf5877aac8
v15hv4 <vishva2912@gmail.com>
2022-12-22 22:38:20 +0530

    I really should've tested before pushing

--- bot/modules/gpt3.py
+++ bot/modules/gpt3.py
@@ -9,7 +9,9 @@ from pyrogram import enums, filters
 # respond using a GPT-3 model
 @app.on_message(filters.mentioned)
 async def gpt3chat(client, message):
-    if message.text.strip()[0] == "@":
+    query = message.text.strip()
+
+    if query[0] == "@":
         query = " ".join(message.text.split()[1:]).strip()
 
     if len(query):
